0e069f4209cb185f4ffcb8185df24095169ce1c9
yzsunjianguo <418406417@qq.com>
2024-08-31 10:45:04 +0800

    code

--- cmd/serverNameExample_httpPbExample/initial/initApp.go
+++ cmd/serverNameExample_httpPbExample/initial/initApp.go
@@ -134,7 +134,7 @@ func InitApp() {
 	if cfg.App.EnableStat {
 		stat.Init(
 			stat.WithLog(logger.Get()),
-			stat.WithAlarm(), // invalid if it is windows, the default threshold for cpu and memory is 0.8, you can modify them
+			stat.WithPrintField(logger.String("service_name", cfg.App.Name), logger.String("host", cfg.App.Host)),
 		)
 		logger.Info("[resource statistics] was initialized")
 	}
