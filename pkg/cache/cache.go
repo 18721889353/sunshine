a82a9aa3b15ff25b9c63e8b935cece0b61311748
yzsunjianguo <418406417@qq.com>
2024-09-19 15:06:53 +0800

    redis loopLock

--- pkg/cache/cache.go
+++ pkg/cache/cache.go
@@ -13,7 +13,7 @@ var (
 	DefaultExpireTime = time.Hour * 24
 	// DefaultNotFoundExpireTime expiry time when result is empty 1 minute,
 	// often used for cache time when data is empty (cache pass-through)
-	DefaultNotFoundExpireTime = time.Minute * 10
+	DefaultNotFoundExpireTime = time.Minute * 1
 	// NotFoundPlaceholder placeholder
 	NotFoundPlaceholder = "*"
 
@@ -26,26 +26,30 @@ var (
 
 // Cache driver interface
 type Cache interface {
-	GetLock(ctx context.Context, key string, timeout time.Duration) (*redsync.Mutex, error)
+	GetLoopLock(ctx context.Context, key string, expireTime, loopWaitTime time.Duration, loopNum int) (*redsync.Mutex, error)
+	GetLock(ctx context.Context, key string, expireTime time.Duration) (*redsync.Mutex, error)
 	ReleaseLock(ctx context.Context, mutex *redsync.Mutex) error
-	Set(ctx context.Context, key string, val interface{}, expiration time.Duration) error
+	Set(ctx context.Context, key string, val interface{}, expireTime time.Duration) error
 	Get(ctx context.Context, key string, val interface{}) error
-	MultiSet(ctx context.Context, valMap map[string]interface{}, expiration time.Duration) error
+	MultiSet(ctx context.Context, valMap map[string]interface{}, expireTime time.Duration) error
 	MultiGet(ctx context.Context, keys []string, valueMap interface{}) error
 	Del(ctx context.Context, keys ...string) error
 	SetCacheWithNotFound(ctx context.Context, key string) error
 }
 
-func GetLock(ctx context.Context, key string, timeout time.Duration) (*redsync.Mutex, error) {
-	return DefaultClient.GetLock(ctx, key, timeout)
+func GetLoopLock(ctx context.Context, key string, expireTime, loopWaitTime time.Duration, loopNum int) (*redsync.Mutex, error) {
+	return DefaultClient.GetLoopLock(ctx, key, expireTime, loopWaitTime, loopNum)
+}
+func GetLock(ctx context.Context, key string, expireTime time.Duration) (*redsync.Mutex, error) {
+	return DefaultClient.GetLock(ctx, key, expireTime)
 }
 func ReleaseLock(ctx context.Context, mutex *redsync.Mutex) error {
 	return DefaultClient.ReleaseLock(ctx, mutex)
 }
 
 // Set data
-func Set(ctx context.Context, key string, val interface{}, expiration time.Duration) error {
-	return DefaultClient.Set(ctx, key, val, expiration)
+func Set(ctx context.Context, key string, val interface{}, expireTime time.Duration) error {
+	return DefaultClient.Set(ctx, key, val, expireTime)
 }
 
 // Get data
@@ -54,8 +58,8 @@ func Get(ctx context.Context, key string, val interface{}) error {
 }
 
 // MultiSet multiple set data
-func MultiSet(ctx context.Context, valMap map[string]interface{}, expiration time.Duration) error {
-	return DefaultClient.MultiSet(ctx, valMap, expiration)
+func MultiSet(ctx context.Context, valMap map[string]interface{}, expireTime time.Duration) error {
+	return DefaultClient.MultiSet(ctx, valMap, expireTime)
 }
 
 // MultiGet multiple get data
