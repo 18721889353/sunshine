4ba9fbd39ebb6a963d8a8eaf75226922fca102b5
sun <418406417@qq.com>
2024-09-19 23:26:05 +0800

    lock

---
+++ pkg/dlock/dlock.go
@@ -0,0 +1,11 @@
+package dlock
+
+import "context"
+
+// Locker is the interface that wraps the basic locking operations.
+type Locker interface {
+	Lock(ctx context.Context) error
+	Unlock(ctx context.Context) error
+	TryLock(ctx context.Context) (bool, error)
+	Close() error
+}
