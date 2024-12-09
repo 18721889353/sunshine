0d2dcb5ed9d9b8cbbfda48fc7f24db62ee758f1e
yzsunjianguo <418406417@qq.com>
2024-12-09 14:54:37 +0800

    etcdcli

--- pkg/etcdcli/option.go
+++ pkg/etcdcli/option.go
@@ -7,46 +7,49 @@ import (
 	"go.uber.org/zap"
 )
 
-// Option set the etcd client options.
+// Option 用于设置 etcd 客户端的选项。
 type Option func(*options)
 
+// options 包含了 etcd 客户端的各种配置选项。
 type options struct {
-	dialTimeout time.Duration // connection timeout, unit(second)
+	dialTimeout time.Duration // 连接超时时间，单位为秒
 
-	username string
-	password string
+	username string // 认证用户名
+	password string // 认证密码
 
-	isSecure           bool
-	serverNameOverride string // etcd domain
-	certFile           string // path to certificate file
+	isSecure           bool   // 是否启用安全模式
+	serverNameOverride string // etcd 域名
+	certFile           string // 证书文件路径
 
-	autoSyncInterval time.Duration // automatic synchronization of member list intervals
-	logger           *zap.Logger
+	autoSyncInterval time.Duration // 成员列表自动同步的时间间隔
+	logger           *zap.Logger   // 日志记录器
 
-	// if you set this parameter, all fields above are invalid
+	// 如果设置了此参数，上述所有字段均无效
 	config *clientv3.Config
 }
 
+// defaultOptions 返回默认的 options 配置。
 func defaultOptions() *options {
 	return &options{
-		dialTimeout: time.Second * 5,
+		dialTimeout: 5 * time.Second, // 默认连接超时时间为 5 秒
 	}
 }
 
+// apply 应用传递的选项到 options 结构体。
 func (o *options) apply(opts ...Option) {
 	for _, opt := range opts {
 		opt(o)
 	}
 }
 
-// WithDialTimeout set dial timeout
+// WithDialTimeout 设置连接超时时间。
 func WithDialTimeout(duration time.Duration) Option {
 	return func(o *options) {
 		o.dialTimeout = duration
 	}
 }
 
-// WithAuth set authentication
+// WithAuth 设置认证信息。
 func WithAuth(username string, password string) Option {
 	return func(o *options) {
 		o.username = username
@@ -54,7 +57,7 @@ func WithAuth(username string, password string) Option {
 	}
 }
 
-// WithSecure set tls
+// WithSecure 设置 TLS 安全连接。
 func WithSecure(serverNameOverride string, certFile string) Option {
 	return func(o *options) {
 		o.isSecure = true
@@ -63,21 +66,21 @@ func WithSecure(serverNameOverride string, certFile string) Option {
 	}
 }
 
-// WithAutoSyncInterval set auto sync interval value
+// WithAutoSyncInterval 设置成员列表自动同步的时间间隔。
 func WithAutoSyncInterval(duration time.Duration) Option {
 	return func(o *options) {
 		o.autoSyncInterval = duration
 	}
 }
 
-// WithLog set logger
+// WithLog 设置日志记录器。
 func WithLog(l *zap.Logger) Option {
 	return func(o *options) {
 		o.logger = l
 	}
 }
 
-// WithConfig set etcd client config
+// WithConfig 设置 etcd 客户端的配置。
 func WithConfig(c *clientv3.Config) Option {
 	return func(o *options) {
 		o.config = c
