ca1a21785632dd8ebfe6feb7a148b2a0d2a98d70
yzsunjianguo <418406417@qq.com>
2025-01-17 17:08:02 +0800

    添加连接rabbitmq 超时时间

--- pkg/rabbitmq/connection.go
+++ pkg/rabbitmq/connection.go
@@ -8,6 +8,7 @@ import (
 	"crypto/tls"
 	"errors"
 	"fmt"
+	"net"
 	"strings"
 	"sync"
 	"time"
@@ -27,8 +28,8 @@ type ConnectionOption func(*connectionOptions)
 type connectionOptions struct {
 	tlsConfig     *tls.Config   // tls config, if the url is amqps this field must be set
 	reconnectTime time.Duration // reconnect time interval, default is 3s
-
-	zapLog *zap.Logger
+	dialTimeout   time.Duration // dial timeout for the connection, default is 5s
+	zapLog        *zap.Logger
 }
 
 func (o *connectionOptions) apply(opts ...ConnectionOption) {
@@ -42,6 +43,7 @@ func defaultConnectionOptions() *connectionOptions {
 	return &connectionOptions{
 		tlsConfig:     nil,
 		reconnectTime: time.Second * 3,
+		dialTimeout:   time.Second * 5, // 设置默认超时时间为5秒
 		zapLog:        defaultLogger,
 	}
 }
@@ -68,6 +70,16 @@ func WithReconnectTime(d time.Duration) ConnectionOption {
 	}
 }
 
+// WithDialTimeout set dial timeout option.
+func WithDialTimeout(d time.Duration) ConnectionOption {
+	return func(o *connectionOptions) {
+		if d == 0 {
+			d = time.Second * 5
+		}
+		o.dialTimeout = d
+	}
+}
+
 // WithLogger set logger option.
 func WithLogger(zapLog *zap.Logger) ConnectionOption {
 	return func(o *connectionOptions) {
@@ -87,6 +99,7 @@ type Connection struct {
 	url           string
 	tlsConfig     *tls.Config
 	reconnectTime time.Duration
+	dialTimeout   time.Duration
 	exit          chan struct{}
 	zapLog        *zap.Logger
 
@@ -109,11 +122,12 @@ func NewConnection(url string, opts ...ConnectionOption) (*Connection, error) {
 		url:           url,
 		reconnectTime: o.reconnectTime,
 		tlsConfig:     o.tlsConfig,
+		dialTimeout:   o.dialTimeout,
 		exit:          make(chan struct{}),
 		zapLog:        o.zapLog,
 	}
 
-	conn, err := connect(connection.url, connection.tlsConfig)
+	conn, err := connect(connection.url, connection.tlsConfig, connection.dialTimeout)
 	if err != nil {
 		return nil, err
 	}
@@ -129,7 +143,7 @@ func NewConnection(url string, opts ...ConnectionOption) (*Connection, error) {
 	return connection, nil
 }
 
-func connect(url string, tlsConfig *tls.Config) (*amqp.Connection, error) {
+func connect(url string, tlsConfig *tls.Config, dialTimeout time.Duration) (*amqp.Connection, error) {
 	var (
 		conn *amqp.Connection
 		err  error
@@ -144,7 +158,11 @@ func connect(url string, tlsConfig *tls.Config) (*amqp.Connection, error) {
 			return nil, err
 		}
 	} else {
-		conn, err = amqp.Dial(url)
+		conn, err = amqp.DialConfig(url, amqp.Config{
+			Dial: func(network, addr string) (net.Conn, error) {
+				return net.DialTimeout(network, addr, dialTimeout) // 使用自定义的超时时间
+			},
+		})
 		if err != nil {
 			return nil, err
 		}
@@ -185,7 +203,7 @@ func (c *Connection) monitor() {
 			c.zapLog.Warn(reconnectTip)
 			time.Sleep(c.reconnectTime) // wait for reconnect
 
-			amqpConn, amqpErr := connect(c.url, c.tlsConfig)
+			amqpConn, amqpErr := connect(c.url, c.tlsConfig, c.dialTimeout)
 			if amqpErr != nil {
 				c.zapLog.Warn("[rabbitmq connection] reconnect failed", zap.String("err", amqpErr.Error()), zap.Int("retryCount", retryCount))
 				continue
