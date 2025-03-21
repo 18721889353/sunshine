c094baa84c7d94a0edf8028aab0ea2536ce8b82d
yzsunjianguo <418406417@qq.com>
2025-03-21 09:32:45 +0800

    up

--- cmd/protoc-gen-go-gin/internal/generate/service/template.go
+++ cmd/protoc-gen-go-gin/internal/generate/service/template.go
@@ -172,7 +172,7 @@ func {{.LowerName}}Router(
 func {{.LowerName}}Middlewares(c *middlewareConfig) {
 	// set up group route middleware, group path is left prefix rules,
 	// if the left prefix is hit, the middleware will take effect, e.g. group route is /api/v1, route /api/v1/{{.LowerName}}/:id  will take effect
-	c.setGroupPath("/api/v1/{{.LowerName}}", middleware.Auth(middleware.WithSwitchHTTPCode(), middleware.WithJwtIgnoreMethods(config.Get().Jwt.IgnoreMethods.HTTP...)))
+	//c.setGroupPath("/api/v1/{{.LowerName}}", middleware.Auth(middleware.WithSwitchHTTPCode(), middleware.WithJwtIgnoreMethods(config.Get().Jwt.IgnoreMethods.HTTP...)))
 
 	// set up single route middleware, just uncomment the code and fill in the middlewares, nothing else needs to be changed
 {{- range .Methods}}
