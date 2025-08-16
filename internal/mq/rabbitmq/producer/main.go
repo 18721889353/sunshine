4a94be2e9276ee5c67491e568e10e3735a7ab47b
yzsunjianguo <418406417@qq.com>
2025-08-16 10:01:19 +0800

    up

--- internal/mq/rabbitmq/producer/main.go
+++ internal/mq/rabbitmq/producer/main.go
@@ -1,26 +1,26 @@
-package main
-
-import (
-	"context"
-	"fmt"
-	"time"
-
-	"github.com/18721889353/sunshine/internal/database"
-)
-
-func main() {
-
-	mqClient, err := database.GetClient(context.Background(), &database.MqCfg{
-		ExchangeName:    "orderExchange",
-		NormalQueueName: "orderQueue",
-		Url:             "amqp://sunjianguo:jianguo123@43.143.78.234:5672/",
-		DialTimeout:     time.Duration(3) * time.Second,
-		IsDeadLetter:    true,
-	})
-	if err != nil {
-		fmt.Println(err)
-		return
-	}
-	err = mqClient.Send(context.Background(), "hello world")
-	fmt.Println(err)
-}
+//package main
+//
+//import (
+//	"context"
+//	"fmt"
+//	"time"
+//
+//	"github.com/18721889353/sunshine/internal/database"
+//)
+//
+//func main() {
+//
+//	mqClient, err := database.GetClient(context.Background(), &database.MqCfg{
+//		ExchangeName:    "orderExchange",
+//		NormalQueueName: "orderQueue",
+//		Url:             "amqp://hello:hello123@127.0.0.1:5672/",
+//		DialTimeout:     time.Duration(3) * time.Second,
+//		IsDeadLetter:    true,
+//	})
+//	if err != nil {
+//		fmt.Println(err)
+//		return
+//	}
+//	err = mqClient.Send(context.Background(), "hello world")
+//	fmt.Println(err)
+//}
