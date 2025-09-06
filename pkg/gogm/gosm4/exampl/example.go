ceaadf935f4d76c1295c4a92c2553975d412843d
yzsunjianguo <418406417@qq.com>
2025-09-06 10:38:58 +0800

    sm

--- pkg/gogm/gosm4/exampl/example.go
+++ pkg/gogm/gosm4/exampl/example.go
@@ -7,14 +7,27 @@ import (
 
 func main() {
 	key := []byte("1234567890123456")
+	iv := []byte("1234567890123456")
+	fmt.Println(string(iv), string(key))
 	data := []byte("hello world")
 	sm4 := gosm4.NewSM4()
-	enData, err := sm4.EncryptECB(data, key).ToHex()
+	//enData, err := sm4.EncryptECB(data, key).ToHex()
+	//if err != nil {
+	//	panic(err)
+	//}
+	//fmt.Println(enData)
+	//bytes, err := sm4.DecryptECBFromHex("a6c66e3894a0b213d4f204f78d6def09", key).ToBytes()
+	//if err != nil {
+	//	panic(err)
+	//}
+	//fmt.Println(string(bytes))
+
+	enData, err := sm4.EncryptCBC(data, key, iv).ToHex()
 	if err != nil {
 		panic(err)
 	}
 	fmt.Println(enData)
-	bytes, err := sm4.DecryptECBFromHex("a6c66e3894a0b213d4f204f78d6def09", key).ToBytes()
+	bytes, err := sm4.DecryptCBCFromHex("80e807ef81b5b9603bf02e9e495b0cbc", key, iv).ToBytes()
 	if err != nil {
 		panic(err)
 	}
