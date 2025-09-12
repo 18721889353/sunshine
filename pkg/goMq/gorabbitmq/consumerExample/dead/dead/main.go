780b12b1bf5f08a66c87478d26d90b29618ae740
yzsunjianguo <418406417@qq.com>
2025-09-12 17:55:50 +0800

    mq

---
+++ pkg/goMq/gorabbitmq/consumerExample/dead/dead/main.go
@@ -0,0 +1,101 @@
+package main
+
+import (
+	"context"
+	"errors"
+	"fmt"
+	"log"
+	"time"
+
+	"github.com/18721889353/sunshine/pkg/logger"
+
+	"github.com/18721889353/sunshine/pkg/goMq/gorabbitmq"
+)
+
+// ProducerExample 生产者使用示例
+func main() {
+	ctx := context.Background()
+	// 创建 RabbitMQ 连接
+	conn, err := gorabbitmq.NewConnection(
+		ctx,
+		"amqp://sunjianguo:jianguo123@43.143.78.234:5672/",
+		gorabbitmq.WithLogger(logger.Get()),
+		gorabbitmq.WithMaxRetries(0), // 无限重试
+		gorabbitmq.WithReconnectTime(2*time.Second),
+		gorabbitmq.WithDialTimeout(5*time.Second),
+	)
+	if err != nil {
+		log.Fatalf("创建连接失败: %v", err)
+	}
+	defer conn.Close()
+	exchangeName := "exchange"
+
+	deadQueueName := "deadQueueName"
+	deadRoutingKey := "deadRoutingKey"
+	normalQueueName := "deadNormalQueueName"
+	normalRoutineKey := "deadNormalRoutingKey"
+	exchange := gorabbitmq.NewDirectExchange(exchangeName, normalRoutineKey)
+	// 创建生产者
+	deadOpts := []gorabbitmq.ConsumerOption{
+		gorabbitmq.WithConsumerDeadLetterOptions(
+			gorabbitmq.WithDeadLetter(exchange.Name(), deadQueueName, deadRoutingKey, normalQueueName, exchange.RoutingKey()),
+			gorabbitmq.WithDeadLetterExchangeDeclareOptions(
+				gorabbitmq.WithExchangeDeclareDurable(true),
+				gorabbitmq.WithExchangeDeclareAutoDelete(false),
+				gorabbitmq.WithExchangeDeclareInternal(false),
+				gorabbitmq.WithExchangeDeclareNoWait(false),
+				gorabbitmq.WithExchangeDeclareArgs(nil),
+			),
+			gorabbitmq.WithDeadLetterDeadQueueDeclareOptions(
+				gorabbitmq.WithQueueDeclareDurable(true),
+				gorabbitmq.WithQueueDeclareExclusive(false),
+				gorabbitmq.WithQueueDeclareAutoDelete(false),
+				gorabbitmq.WithQueueDeclareNoWait(false),
+				gorabbitmq.WithQueueDeclareArgs(map[string]interface{}{
+					"x-dead-letter-exchange":    exchange.Name(),
+					"x-dead-letter-routing-key": normalRoutineKey,
+					"x-message-ttl":             10000,
+				})),
+			gorabbitmq.WithDeadLetterDeadQueueBindOptions(
+				gorabbitmq.WithQueueBindNoWait(false),
+				gorabbitmq.WithQueueBindArgs(nil),
+			),
+			gorabbitmq.WithDeadLetterNormalQueueDeclareOptions(
+				gorabbitmq.WithQueueDeclareDurable(true),
+				gorabbitmq.WithQueueDeclareExclusive(false),
+				gorabbitmq.WithQueueDeclareAutoDelete(false),
+				gorabbitmq.WithQueueDeclareNoWait(false),
+				gorabbitmq.WithQueueDeclareArgs(map[string]interface{}{
+					"x-dead-letter-exchange":    exchange.Name(),
+					"x-dead-letter-routing-key": deadRoutingKey,
+				})),
+			gorabbitmq.WithDeadLetterNormalQueueBindOptions(
+				gorabbitmq.WithQueueBindNoWait(false),
+				gorabbitmq.WithQueueBindArgs(nil),
+			),
+		),
+		gorabbitmq.WithConsumerQosOptions(
+			gorabbitmq.WithQosEnable(),
+			gorabbitmq.WithQosPrefetchCount(1),
+			gorabbitmq.WithQosPrefetchSize(0),
+			gorabbitmq.WithQosPrefetchGlobal(false),
+		),
+		gorabbitmq.WithConsumerConsumeOptions(
+			gorabbitmq.WithConsumeConsumer("hello"),
+			gorabbitmq.WithConsumeExclusive(false),
+			gorabbitmq.WithConsumeNoLocal(false),
+			gorabbitmq.WithConsumeNoWait(false),
+			gorabbitmq.WithConsumeArgs(nil),
+		),
+		gorabbitmq.WithConsumerAutoAck(false),
+		gorabbitmq.WithConsumerMsgDurable(true),
+	}
+	consumer, err := gorabbitmq.NewConsumer(exchange, normalQueueName, conn, deadOpts...)
+	consumer.Consume(ctx, func(ctx context.Context, data []byte, tagID string) error {
+		fmt.Println(string(data))
+		fmt.Println(tagID)
+		return errors.New("fuck")
+	})
+	forever := make(chan struct{})
+	<-forever
+}
