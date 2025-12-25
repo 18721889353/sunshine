544346f9bd4dece6852e373a9b867e1eeee2d8e2
yzsunjianguo <418406417@qq.com>
2025-12-25 16:34:19 +0800

    up

--- internal/routers/routers_pbExample.go
+++ internal/routers/routers_pbExample.go
@@ -34,37 +34,24 @@ var (
 	allMiddlewareFns = []func(c *middlewareConfig){}
 )
 
-//// 对象池用于重用CpDealerApiLog实例
-//var cpDealerApiLogPool = sync.Pool{
-//	New: func() interface{} {
-//		return &model.CpDealerApiLog{}
-//	},
-//}
-//
 //func customLogFunc(c *gin.Context, reqBody []byte, respBody []byte, startTime time.Time, endTime time.Time, spendTime int64) {
 //	go func() {
-//		// 从池中获取CpDealerApiLog实例
-//		log := cpDealerApiLogPool.Get().(*model.CpDealerApiLog)
-//		// 重置字段值
-//		log.Type = "接口"
-//		log.Category = "API"
-//		log.IP = c.ClientIP()
-//		log.Url = c.Request.URL.String()
-//		log.Params = string(reqBody)
-//		log.Response = string(respBody)
-//		log.StartTime = cast.ToString(startTime.UnixMilli())
-//		log.EndTime = cast.ToString(endTime.UnixMilli())
-//		log.SpendTime = cast.ToString(spendTime)
-//		log.DealerID = cast.ToInt(c.GetString("uid"))
-//		log.Active = "golang api"
-//		log.CreateTime = cast.ToString(time.Now().Unix())
-//		log.UpdateTime = int(time.Now().Unix())
-//
 //		// 保存到数据库
-//		database.GetDB().Create(log)
-//
-//		// 使用完毕后将对象放回池中
-//		cpDealerApiLogPool.Put(log)
+//		database.GetDB().Create(&model.CpDealerApiLog{
+//			Type:       "接口",
+//			Category:   "API",
+//			IP:         c.ClientIP(),
+//			Url:        c.Request.URL.String(),
+//			Params:     string(reqBody),
+//			Response:   string(respBody),
+//			StartTime:  cast.ToString(startTime.UnixMilli()),
+//			EndTime:    cast.ToString(endTime.UnixMilli()),
+//			SpendTime:  cast.ToString(spendTime),
+//			DealerID:   cast.ToInt(c.GetString("uid")),
+//			Active:     "golang api",
+//			CreateTime: cast.ToString(time.Now().Unix()),
+//			UpdateTime: int(time.Now().Unix()),
+//		})
 //	}()
 //}
 
