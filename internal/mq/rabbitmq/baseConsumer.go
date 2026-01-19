2f7871b0d5bb2b1a3e27706ed7451da3afe074ec
yzsunjianguo <418406417@qq.com>
2026-01-19 18:40:20 +0800

    mq up

--- internal/mq/rabbitmq/baseConsumer.go
+++ internal/mq/rabbitmq/baseConsumer.go
@@ -2,11 +2,10 @@ package mq
 
 import (
 	"context"
-	"strconv"
-	"sync"
-
 	"github.com/18721889353/sunshine/internal/config"
 	"github.com/18721889353/sunshine/internal/database"
+	"strconv"
+	"sync"
 
 	"github.com/18721889353/sunshine/pkg/goMq/gorabbitmq"
 	"github.com/18721889353/sunshine/pkg/logger"
@@ -18,6 +17,8 @@ type BaseConsumer struct {
 	stopConsumeChan chan struct{}
 	wg              sync.WaitGroup
 	handler         MessageHandler
+	consumers       []*gorabbitmq.Consumer
+	consumersMutex  sync.RWMutex
 }
 
 // MessageHandler 定义消息处理函数类型
@@ -44,6 +45,130 @@ func (bc *BaseConsumer) handleMessage(ctx context.Context, data []byte, messageI
 	return bc.handler(ctx, data, messageId, tagID)
 }
 
+// 辅助方法：构建基础选项
+func (bc *BaseConsumer) prepareOptions(queueConfig config.DoingOrder, index int) []gorabbitmq.ConsumerOption {
+	consumerName := queueConfig.ConsumerOption.Consumer
+	if consumerName == "" {
+		consumerName = "consumer"
+	}
+	consumerName += "_" + strconv.Itoa(index+1)
+
+	return []gorabbitmq.ConsumerOption{
+		gorabbitmq.WithConsumerQosOptions(
+			gorabbitmq.WithQosEnable(),
+			gorabbitmq.WithQosPrefetchCount(queueConfig.ConsumerOption.PrefetchCount),
+			gorabbitmq.WithQosPrefetchSize(queueConfig.ConsumerOption.PrefetchSize),
+			gorabbitmq.WithQosPrefetchGlobal(queueConfig.ConsumerOption.Global),
+		),
+		gorabbitmq.WithConsumerMsgDurable(queueConfig.ConsumerOption.MsgDurable),
+		gorabbitmq.WithConsumerAutoAck(queueConfig.ConsumerOption.IsAutoAck),
+		gorabbitmq.WithConsumerConsumeOptions(
+			gorabbitmq.WithConsumeConsumer(consumerName),
+			gorabbitmq.WithConsumeExclusive(queueConfig.ConsumerOption.Exclusive),
+			gorabbitmq.WithConsumeNoLocal(queueConfig.ConsumerOption.NoLocal),
+			gorabbitmq.WithConsumeNoWait(queueConfig.ConsumerOption.NoWait),
+		),
+	}
+}
+
+// buildBaseOptions 构建通用的消费者配置
+func (bc *BaseConsumer) buildBaseOptions(cfg config.DoingOrder, index int) []gorabbitmq.ConsumerOption {
+	name := cfg.ConsumerOption.Consumer
+	if name == "" {
+		name = "consumer"
+	}
+	name += strconv.Itoa(index + 1)
+
+	return []gorabbitmq.ConsumerOption{
+		// 为每个消费者构建独立的选项
+		gorabbitmq.WithConsumerQosOptions(
+			gorabbitmq.WithQosEnable(),
+			gorabbitmq.WithQosPrefetchCount(cfg.ConsumerOption.PrefetchCount),
+			gorabbitmq.WithQosPrefetchSize(cfg.ConsumerOption.PrefetchSize),
+			gorabbitmq.WithQosPrefetchGlobal(cfg.ConsumerOption.Global),
+		),
+		gorabbitmq.WithConsumerMsgDurable(cfg.ConsumerOption.MsgDurable),
+		gorabbitmq.WithConsumerAutoAck(cfg.ConsumerOption.IsAutoAck),
+		// 添加消费选项
+		gorabbitmq.WithConsumerConsumeOptions(
+			gorabbitmq.WithConsumeConsumer(name),
+			gorabbitmq.WithConsumeExclusive(cfg.ConsumerOption.Exclusive),
+			gorabbitmq.WithConsumeNoLocal(cfg.ConsumerOption.NoLocal),
+			gorabbitmq.WithConsumeNoWait(cfg.ConsumerOption.NoWait),
+			gorabbitmq.WithConsumeArgs(nil),
+		),
+	}
+}
+
+// buildDeadLetterOptions 封装死信队列选项
+func (bc *BaseConsumer) buildDeadLetterOptions(exchange *gorabbitmq.Exchange, cfg config.DoingOrder, deadQueueName, deadRoutingKey, normalQueueName, normalRoutineKey string) []gorabbitmq.ConsumerOption {
+	return []gorabbitmq.ConsumerOption{
+		gorabbitmq.WithConsumerDeadLetterOptions(
+			gorabbitmq.WithDeadLetter(exchange.Name(), deadQueueName, deadRoutingKey, normalQueueName, exchange.RoutingKey()),
+			gorabbitmq.WithDeadLetterExchangeDeclareOptions(
+				gorabbitmq.WithExchangeDeclareDurable(cfg.ExchangeDeclareOptions.Durable),
+				gorabbitmq.WithExchangeDeclareAutoDelete(cfg.ExchangeDeclareOptions.AutoDelete),
+				gorabbitmq.WithExchangeDeclareInternal(cfg.ExchangeDeclareOptions.Internal),
+				gorabbitmq.WithExchangeDeclareNoWait(cfg.ExchangeDeclareOptions.NoWait),
+				gorabbitmq.WithExchangeDeclareArgs(nil),
+			),
+			gorabbitmq.WithDeadLetterDeadQueueDeclareOptions(
+				gorabbitmq.WithQueueDeclareDurable(cfg.DeadQueueDeclareOption.Durable),
+				gorabbitmq.WithQueueDeclareExclusive(cfg.DeadQueueDeclareOption.Exclusive),
+				gorabbitmq.WithQueueDeclareAutoDelete(cfg.DeadQueueDeclareOption.AutoDelete),
+				gorabbitmq.WithQueueDeclareNoWait(cfg.DeadQueueDeclareOption.NoWait),
+				gorabbitmq.WithQueueDeclareArgs(map[string]interface{}{
+					"x-dead-letter-exchange":    exchange.Name(),
+					"x-dead-letter-routing-key": normalRoutineKey,
+					"x-message-ttl":             cfg.DeadQueueDeclareOption.Args.XMessageTTL,
+				})),
+			gorabbitmq.WithDeadLetterDeadQueueBindOptions(
+				gorabbitmq.WithQueueBindNoWait(cfg.DeadQueueBindOption.NoWait),
+				gorabbitmq.WithQueueBindArgs(nil),
+			),
+			gorabbitmq.WithDeadLetterNormalQueueDeclareOptions(
+				gorabbitmq.WithQueueDeclareDurable(cfg.NormalQueueDeclareOption.Durable),
+				gorabbitmq.WithQueueDeclareExclusive(cfg.NormalQueueDeclareOption.Exclusive),
+				gorabbitmq.WithQueueDeclareAutoDelete(cfg.NormalQueueDeclareOption.AutoDelete),
+				gorabbitmq.WithQueueDeclareNoWait(cfg.NormalQueueDeclareOption.NoWait),
+				gorabbitmq.WithQueueDeclareArgs(map[string]interface{}{
+					"x-dead-letter-exchange":    exchange.Name(),
+					"x-dead-letter-routing-key": deadRoutingKey,
+				})),
+			gorabbitmq.WithDeadLetterNormalQueueBindOptions(
+				gorabbitmq.WithQueueBindNoWait(cfg.NormalQueueBindOption.NoWait),
+				gorabbitmq.WithQueueBindArgs(nil),
+			),
+		),
+	}
+}
+
+// buildNormalLetterOptions 封装普通队列选项
+func (bc *BaseConsumer) buildNormalLetterOptions(exchange *gorabbitmq.Exchange, cfg config.DoingOrder, normalQueueName string) []gorabbitmq.ConsumerOption {
+	return []gorabbitmq.ConsumerOption{
+		gorabbitmq.WithConsumerNormalLetterOptions(
+			gorabbitmq.WithNormalLetter(exchange.Name(), normalQueueName, exchange.RoutingKey()),
+			gorabbitmq.WithNormalLetterExchangeDeclareOptions(
+				gorabbitmq.WithExchangeDeclareDurable(cfg.ExchangeDeclareOptions.Durable),
+				gorabbitmq.WithExchangeDeclareAutoDelete(cfg.ExchangeDeclareOptions.AutoDelete),
+				gorabbitmq.WithExchangeDeclareInternal(cfg.ExchangeDeclareOptions.Internal),
+				gorabbitmq.WithExchangeDeclareNoWait(cfg.ExchangeDeclareOptions.NoWait),
+				gorabbitmq.WithExchangeDeclareArgs(nil),
+			),
+			gorabbitmq.WithNormalLetterNormalQueueDeclareOptions(
+				gorabbitmq.WithQueueDeclareDurable(cfg.NormalQueueDeclareOption.Durable),
+				gorabbitmq.WithQueueDeclareExclusive(cfg.NormalQueueDeclareOption.Exclusive),
+				gorabbitmq.WithQueueDeclareAutoDelete(cfg.NormalQueueDeclareOption.AutoDelete),
+				gorabbitmq.WithQueueDeclareNoWait(cfg.NormalQueueDeclareOption.NoWait),
+				gorabbitmq.WithQueueDeclareArgs(nil),
+			),
+			gorabbitmq.WithNormalLetterNormalQueueBindOptions(
+				gorabbitmq.WithQueueBindNoWait(cfg.NormalQueueBindOption.NoWait),
+				gorabbitmq.WithQueueBindArgs(nil),
+			)),
+	}
+}
+
 // Start 启动消费者
 func (bc *BaseConsumer) Start(queueConfig config.DoingOrder, queueType string) error {
 	ctx := context.Background()
@@ -58,140 +183,67 @@ func (bc *BaseConsumer) Start(queueConfig config.DoingOrder, queueType string) e
 			normalQueueName := queueConfig.NormalQueueName
 			normalRoutineKey := exchangeName + "." + normalQueueName
 			exchange := gorabbitmq.NewDirectExchange(exchangeName, normalRoutineKey)
-			queueOptions := queueConfig
 			// 获取需要启动的消费者数量
-			consumerNum := queueOptions.ConsumerNum
+			consumerNum := queueConfig.ConsumerNum
 			if consumerNum <= 0 {
 				consumerNum = 1
 			}
 			// 创建指定数量的消费者
 			for i := 0; i < consumerNum; i++ {
 				// 为每个消费者创建独立的函数作用域，确保每个消费者使用独立的连接
-				func(consumerIndex int) {
-					connection, err := mqObject.GetConnection(ctx)
-					if err != nil {
-						logger.Panic("database.GetRabbitMQ().GetConnection error", logger.Err(err))
-					}
-					defer mqObject.PutConnection(ctx, connection) // 将连接放回连接池
+				connection, err := mqObject.GetConnection(ctx)
+				if err != nil {
+					logger.Panic("database.GetRabbitMQ().GetConnection error", logger.Err(err))
+				}
+				defer mqObject.PutConnection(ctx, connection)
+				// 构建基础选项
+				consumerOpts := bc.buildBaseOptions(queueConfig, i)
 
-					// 为每个消费者构建独立的选项
-					consumerOpts := []gorabbitmq.ConsumerOption{
-						gorabbitmq.WithConsumerQosOptions(
-							gorabbitmq.WithQosEnable(),
-							gorabbitmq.WithQosPrefetchCount(queueOptions.ConsumerOption.PrefetchCount),
-							gorabbitmq.WithQosPrefetchSize(queueOptions.ConsumerOption.PrefetchSize),
-							gorabbitmq.WithQosPrefetchGlobal(queueOptions.ConsumerOption.Global),
-						),
-						gorabbitmq.WithConsumerMsgDurable(queueOptions.ConsumerOption.MsgDurable),
-						gorabbitmq.WithConsumerAutoAck(queueOptions.ConsumerOption.IsAutoAck),
-					}
+				if queueType == "dead" {
+					consumerOpts = append(consumerOpts, bc.buildDeadLetterOptions(exchange, queueConfig, deadQueueName, deadRoutingKey, normalQueueName, normalRoutineKey)...)
+				}
+				if queueType == "normal" {
+					consumerOpts = append(consumerOpts, bc.buildNormalLetterOptions(exchange, queueConfig, normalQueueName)...)
+				}
 
-					// 设置消费者的唯一名称
-					consumerName := queueOptions.ConsumerOption.Consumer
-					if consumerName == "" {
-						consumerName = "consumer"
-					}
-					consumerName += strconv.Itoa(consumerIndex + 1)
+				consumer, err := gorabbitmq.NewConsumer(exchange, normalQueueName, connection, consumerOpts...)
+				if err != nil {
+					logger.Panic("异步消息队列 failed to create rabbitmq consumer error", logger.Err(err))
+				}
+				bc.consumersMutex.Lock()
+				bc.consumers = append(bc.consumers, consumer)
+				bc.consumersMutex.Unlock()
 
-					// 添加消费选项
-					consumerOpts = append(consumerOpts, gorabbitmq.WithConsumerConsumeOptions(
-						gorabbitmq.WithConsumeConsumer(consumerName),
-						gorabbitmq.WithConsumeExclusive(queueOptions.ConsumerOption.Exclusive),
-						gorabbitmq.WithConsumeNoLocal(queueOptions.ConsumerOption.NoLocal),
-						gorabbitmq.WithConsumeNoWait(queueOptions.ConsumerOption.NoWait),
-						gorabbitmq.WithConsumeArgs(nil),
-					))
-
-					if queueType == "dead" {
-						consumerOpts = append(consumerOpts,
-							gorabbitmq.WithConsumerDeadLetterOptions(
-								gorabbitmq.WithDeadLetter(exchange.Name(), deadQueueName, deadRoutingKey, normalQueueName, exchange.RoutingKey()),
-								gorabbitmq.WithDeadLetterExchangeDeclareOptions(
-									gorabbitmq.WithExchangeDeclareDurable(queueOptions.ExchangeDeclareOptions.Durable),
-									gorabbitmq.WithExchangeDeclareAutoDelete(queueOptions.ExchangeDeclareOptions.AutoDelete),
-									gorabbitmq.WithExchangeDeclareInternal(queueOptions.ExchangeDeclareOptions.Internal),
-									gorabbitmq.WithExchangeDeclareNoWait(queueOptions.ExchangeDeclareOptions.NoWait),
-									gorabbitmq.WithExchangeDeclareArgs(nil),
-								),
-								gorabbitmq.WithDeadLetterDeadQueueDeclareOptions(
-									gorabbitmq.WithQueueDeclareDurable(queueOptions.DeadQueueDeclareOption.Durable),
-									gorabbitmq.WithQueueDeclareExclusive(queueOptions.DeadQueueDeclareOption.Exclusive),
-									gorabbitmq.WithQueueDeclareAutoDelete(queueOptions.DeadQueueDeclareOption.AutoDelete),
-									gorabbitmq.WithQueueDeclareNoWait(queueOptions.DeadQueueDeclareOption.NoWait),
-									gorabbitmq.WithQueueDeclareArgs(map[string]interface{}{
-										"x-dead-letter-exchange":    exchange.Name(),
-										"x-dead-letter-routing-key": normalRoutineKey,
-										"x-message-ttl":             queueOptions.DeadQueueDeclareOption.Args.XMessageTTL,
-									})),
-								gorabbitmq.WithDeadLetterDeadQueueBindOptions(
-									gorabbitmq.WithQueueBindNoWait(queueOptions.DeadQueueBindOption.NoWait),
-									gorabbitmq.WithQueueBindArgs(nil),
-								),
-								gorabbitmq.WithDeadLetterNormalQueueDeclareOptions(
-									gorabbitmq.WithQueueDeclareDurable(queueOptions.NormalQueueDeclareOption.Durable),
-									gorabbitmq.WithQueueDeclareExclusive(queueOptions.NormalQueueDeclareOption.Exclusive),
-									gorabbitmq.WithQueueDeclareAutoDelete(queueOptions.NormalQueueDeclareOption.AutoDelete),
-									gorabbitmq.WithQueueDeclareNoWait(queueOptions.NormalQueueDeclareOption.NoWait),
-									gorabbitmq.WithQueueDeclareArgs(map[string]interface{}{
-										"x-dead-letter-exchange":    exchange.Name(),
-										"x-dead-letter-routing-key": deadRoutingKey,
-									})),
-								gorabbitmq.WithDeadLetterNormalQueueBindOptions(
-									gorabbitmq.WithQueueBindNoWait(queueOptions.NormalQueueBindOption.NoWait),
-									gorabbitmq.WithQueueBindArgs(nil),
-								),
-							))
-					}
-					if queueType == "normal" {
-						consumerOpts = append(consumerOpts,
-							gorabbitmq.WithConsumerNormalLetterOptions(
-								gorabbitmq.WithNormalLetter(exchange.Name(), normalQueueName, exchange.RoutingKey()),
-								gorabbitmq.WithNormalLetterExchangeDeclareOptions(
-									gorabbitmq.WithExchangeDeclareDurable(queueOptions.ExchangeDeclareOptions.Durable),
-									gorabbitmq.WithExchangeDeclareAutoDelete(queueOptions.ExchangeDeclareOptions.AutoDelete),
-									gorabbitmq.WithExchangeDeclareInternal(queueOptions.ExchangeDeclareOptions.Internal),
-									gorabbitmq.WithExchangeDeclareNoWait(queueOptions.ExchangeDeclareOptions.NoWait),
-									gorabbitmq.WithExchangeDeclareArgs(nil),
-								),
-								gorabbitmq.WithNormalLetterNormalQueueDeclareOptions(
-									gorabbitmq.WithQueueDeclareDurable(queueOptions.NormalQueueDeclareOption.Durable),
-									gorabbitmq.WithQueueDeclareExclusive(queueOptions.NormalQueueDeclareOption.Exclusive),
-									gorabbitmq.WithQueueDeclareAutoDelete(queueOptions.NormalQueueDeclareOption.AutoDelete),
-									gorabbitmq.WithQueueDeclareNoWait(queueOptions.NormalQueueDeclareOption.NoWait),
-									gorabbitmq.WithQueueDeclareArgs(nil),
-								),
-								gorabbitmq.WithNormalLetterNormalQueueBindOptions(
-									gorabbitmq.WithQueueBindNoWait(queueOptions.NormalQueueBindOption.NoWait),
-									gorabbitmq.WithQueueBindArgs(nil),
-								),
-							))
-					}
-
-					consumer, err := gorabbitmq.NewConsumer(exchange, normalQueueName, connection, consumerOpts...)
-					if err != nil {
-						logger.Panic("异步消息队列 failed to create rabbitmq consumer error", logger.Err(err))
-					}
-					consumer.Consume(ctx, bc.handleMessage)
-					logger.Info("消息队列 " + queueConfig.NormalQueueName + " 第" + strconv.Itoa(consumerIndex+1) + "个消费者(" + consumerName + ")启动成功")
-				}(i)
+				// 启动异步消费 (底层 consumer.go)
+				consumer.Consume(ctx, bc.handleMessage)
+				logger.Info("队列 " + normalQueueName + " 消费者 " + strconv.Itoa(i+1) + " 已启动")
 			}
 			logger.Info("消息队列 " + queueConfig.NormalQueueName + " 总共启动了 " + strconv.Itoa(consumerNum) + " 个消费者")
 		}
 		<-bc.stopConsumeChan
 
-		logger.Warn(bc.name + " goroutine 退出")
+		logger.Warn(bc.name + " 主监听协程已通过 Context 退出")
 	}()
 	return nil
 }
 
 // Stop 停止消费者
 func (bc *BaseConsumer) Stop() error {
-	logger.Warn("开始停止" + bc.name)
+	logger.Warn(">>> 接收到停止指令: " + bc.name)
 	if bc.stopConsumeChan != nil {
 		close(bc.stopConsumeChan)
 	}
-	// 等待所有正在处理的消息完成
+	bc.consumersMutex.Lock()
+	defer bc.consumersMutex.Unlock()
+	for _, consumer := range bc.consumers {
+		if consumer != nil {
+			consumer.Close()
+			logger.Info("成功停止" + consumer.QueueName)
+		}
+	}
+
+	// BaseConsumer 层的业务逻辑处理完毕
 	bc.wg.Wait()
-	logger.Warn("成功停止" + bc.name)
+	logger.Warn("<<< 消费者服务已安全停止: " + bc.name)
 	return nil
 }
