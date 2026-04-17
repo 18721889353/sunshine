be7ad134a782e9d450226bd11ba271a5c185d6a3
yzsunjianguo <418406417@qq.com>
2026-04-17 14:08:26 +0800

    clean

--- pkg/utils/dsn_test.go
+++ pkg/utils/dsn_test.go
@@ -4,25 +4,6 @@ import (
 	"testing"
 )
 
-func TestAdaptivePostgresqlDsn(t *testing.T) {
-	pgDsns := []string{
-		"postgres://root:123456@192.168.3.37:5432/account",
-		"postgres://root:123456@192.168.3.37:5432/account?sslmode=disable",
-		"postgres://root:123456@192.168.3.37:5432/account?TimeZone=Asia/Shanghai",
-		"root:123456@192.168.3.37:5432/account?search_path=public",
-		"host=192.168.3.37 port=5432 user=root password=123456 dbname=account sslmode=disable",
-
-		"root:123456@(192.168.3.37:5432)/account",
-		"postgres://root:123456@(192.168.3.37:5432)/account",
-		"postgres://root:123456@(192.168.3.37:5432)/account?TimeZone=Asia/Shanghai",
-	}
-
-	for _, v := range pgDsns {
-		dsn := AdaptivePostgresqlDsn(v)
-		t.Log(dsn)
-	}
-}
-
 func TestAdaptiveMysqlDsn(t *testing.T) {
 	mysqlDsns := []string{
 		"root:123456@(192.168.3.37:3306)/account",
